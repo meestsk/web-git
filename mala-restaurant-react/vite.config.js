ceb87bf9aed6b47e337d9413780ce59b9a4e4aa6
unknown <meestsk>
2025-09-21 04:19:47 +0700

    update

--- mala-restaurant-react/vite.config.js
+++ mala-restaurant-react/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,      // เปิดให้เครื่องอื่นใน LAN เข้าได้ (ถ้าต้องการ)
-    open: true,      // เปิดเบราว์เซอร์อัตโนมัติ
-    port: 5173       // จะเปลี่ยนพอร์ตก็ได้
-    // open: '/dashboard' // ถ้าอยากเปิด path เฉพาะ
+    host: true,      
+    open: true,      
+    port: 5173       
+    
   }
 })
