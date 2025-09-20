6d903fb75895066435dae6379fb57e1323aeb5d3
meestsk <108624231+meestsk@users.noreply.github.com>
2025-09-21 03:29:22 +0700

    Update vite.config.js

--- mala-restaurant-react/vite.config.js
+++ mala-restaurant-react/vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { 
-    host: true,    // host:true = 0.0.0.0
-    port: 5173 
+  server: {
+    host: true,      
+    open: true,      
+    port: 5173       
+    
   }
 })
